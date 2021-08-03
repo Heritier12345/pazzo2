b1f29769f5c668c049539dc6df3cd3d4a68ed972
Heritier12345 <87946128+Heritier12345@users.noreply.github.com>
2021-08-03 13:50:40 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KIMENYI Heritier</title>
+</head>
+<frameset rows="15%,85%">
+    <frame src="title.html" name="title">
+        <frameset cols="65%,35%">
+            <frame src="myinfo.html" name="title">
+                <frame src="media.html" name="media">
+        </frameset>
+</frameset>
+</html>
